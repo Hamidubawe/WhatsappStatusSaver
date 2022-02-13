b93d65d41c0d599864c1df1248189695e7b0c8b6
Hamidu Bawee <Hamidubawe@gmail.com>
2022-02-13 10:59:15 +0100

    Update Constants.kt

--- app/src/main/java/com/freestatusaver/Constants.kt
+++ app/src/main/java/com/freestatusaver/Constants.kt
@@ -7,6 +7,6 @@ class Constants {
         val ANDROID10 = "/Android/"
         const val BUSINESS_FOLDER_NAME = "/WhatsApp Business/"
         const val SAVE_FOLDER = "/WhatsApp Status/"
-        const val InsterstialAd = "ca-app-pub-9047571753426746/6995255825"
+        const val InsterstialAd = "your ad unit"
     }
 }
